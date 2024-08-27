8dd4aed5c6b48bd00602b87027a221c5273a3f45
hafidznasution <rhafis709@gmail.com>
2024-08-27 12:01:39 +0700

    6 penulisan komentar php

---
+++ 6 penulisan komentar php.php
@@ -0,0 +1,15 @@
+<?php 
+
+// ini adalah komentar
+echo "Hello nama saya hafis";
+
+/*
+ini adalah komentar 
+yang lebih dari satu 
+baris
+*/
+
+
+echo "<br><br><br><strong>by M HAFIDZ RAMADHAN NST</strong>";
+
+?>
