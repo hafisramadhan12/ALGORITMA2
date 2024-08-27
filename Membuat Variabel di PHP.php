8b6375a030d974760a9e4321ee85d8b0d394ad8b
hafidznasution <rhafis709@gmail.com>
2024-08-27 12:54:13 +0700

    membuat variabel di php

---
+++ Membuat Variabel di PHP.php
@@ -0,0 +1,53 @@
+<?php
+
+$nisn1 = "0083825885";
+$harga = 20000;
+$nama_barang = "daging wagyu";
+$NamaBarang1          = "daging";
+$harga = 10.0000000;
+$stok = 70;
+// membuat variabel baru
+$stok = 70;
+
+// mengisi ulang variabel dengan nilai baru
+$stok = 84;
+
+// Contoh: Salah
+// $!nama = "hafis";
+// $46rosi = "hafiscode";
+// Contoh: Benar
+$nama = "hafidz";
+$rosi46 = "hafidz code";
+
+$nama_barang = "daging wagyu";
+$namaPembeli = "ilham"; // menggunakan camelCase
+
+// Contoh barang dan pembeli:
+$nama_barang = "asus rog";
+$namaPembeli = "riyan";
+$harga = 70000;
+
+
+// Contoh: html css php.
+$Belajar = "html";
+$BELAJAR = "css";
+$belajar = "php";
+
+// =================== cara mencetak =======================
+echo 
+$nisn1,"<p>".
+$harga,"<p>".
+$nama_barang,"<p>".
+$NamaBarang1,"<p>".
+$harga,"<p>".
+$stok,"<p>".
+$nama,"<p>".
+$rosi46,"<p>".
+$nama_barang,"<p>".
+$namaPembeli,"<p>". 
+$nama_barang,"<p>".
+$namaPembeli,"<p>".
+$harga,"<p>".
+$Belajar,"<p>".
+$BELAJAR,"<p>".
+$belajar;
