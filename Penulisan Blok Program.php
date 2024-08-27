2fb7ff8d3c13b26865e5837e1a2f008b01679b5b
hafidznasution <rhafis709@gmail.com>
2024-08-27 12:17:26 +0700

    Penulisan Blok Program

---
+++ Penulisan Blok Program.php
@@ -0,0 +1,9 @@
+<?php
+$umur=16;{
+    echo "<p>Kamu sudah dewasa";
+    echo "<p>Selamat datang";
+    echo "<p>kamu boleh main game";
+    
+echo "<br><br><br><strong>by M HAFIDZ RAMADHAN NST</strong>";
+
+}
