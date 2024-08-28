b61c8626ad0f80b0b61cbc99177fe5be3e7c973e
hafidznasution <rhafis709@gmail.com>
2024-08-28 08:17:24 +0700

    mencetak nlai dari variabel php

---
+++ 11 Mencetak Nilai dari Variabe Pada PHP.php
@@ -0,0 +1,10 @@
+<?php
+
+// membuat variabel baru
+$nama_barang = "Mi buk ay";
+$harga = 5000;
+
+// menampilkan isi variabel
+echo "Ibu membeli $nama_barang seharga Rp $harga";
+
+echo "<br><br><br><strong>by M HAFIDZ RAMADHAN NST!";
