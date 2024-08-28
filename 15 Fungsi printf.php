85f650a754f74f60310b6eadc8463b28250f7ba3
hafidznasution <rhafis709@gmail.com>
2024-08-28 09:11:02 +0700

    fungsi printif php

---
+++ 15 Fungsi printf.php
@@ -0,0 +1,11 @@
+<?php
+// misalkan kita punya bilangan dengan notasi E seperti ini
+$harga = 200000;
+
+// jika kita cetak dengan echo:
+echo "Harganya adalah Rp $harga";
+
+// jika kita cetak dengan printf
+printf("Harganya adalah Rp %.2f", $harga);
+
+echo "<br><br><br><strong>by M HAFIDZ RAMADHAN NST!";
