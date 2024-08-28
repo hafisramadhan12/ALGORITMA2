dfb83b0c6cdf2d0e021f8e099f96f76181c24278
hafidznasution <rhafis709@gmail.com>
2024-08-28 09:43:30 +0700

    tipe data integar di php

---
+++ 19 Tipe Data Integer di PHP.php
@@ -0,0 +1,8 @@
+<?php
+$nilai = 37; // angka positif
+$poin = -55; // angka negatif
+
+echo "angka positif $nilai";
+echo "angka negatif $poin";
+
+echo "<br><br><br><strong>by M HAFIDZ RAMADHAN NST!";
