0344465a70dcf3aa968f198c1a037b22c76d3248
hafidznasution <rhafis709@gmail.com>
2024-08-28 09:51:48 +0700

    tipe data float

---
+++ 20 Tipe Data Float di PHP.php
@@ -0,0 +1,11 @@
+<?php
+$panjang = 133.12;
+$kecepatan = 1328.12;
+$jarak = 13232*12356.12;
+
+//batasi angka di belakang koma
+echo "<p>nilai jarak: $jarak";
+echo "<p>nilai kecepatan: $kecepatan";
+echo "<p>nilai panjang: $panjang";
+
+echo "<br><br><br><strong>by M HAFIDZ RAMADHAN NST!";
