02d4c95318588894792abd9afab84377e26a4054
hafidznasution <rhafis709@gmail.com>
2024-08-28 10:17:25 +0700

    tipe data boolean do php

---
+++ 21 Tipe data Boolean di PHP.php
@@ -0,0 +1,19 @@
+<?php
+//mendeklarasikan variabel boolean
+$isActive = false;
+$menikah = true;
+//menggunakan boolean dalam pernyataan if
+if ($isActive){
+    echo "nilai ini benar.";
+} else{
+    echo "nilai ini salah.";
+}
+
+//contoh lain mengunakan boolean 
+$condition = (5 > 2);
+if ($condition) {
+    echo "<p>5 lebih besar dari 2.";
+} else{
+    echo "5 lebih besar dari 2.";
+}
+echo "<br><br><br><strong>by M HAFIDZ RAMADHAN NST!";
