fd13e46a6d4921e3260e42f5e7ccd5bc5a8e3ce4
hafidznasution <rhafis709@gmail.com>
2024-08-28 08:20:14 +0700

    fungsi echo

---
+++ 12 Fungsi echo().php
@@ -0,0 +1,8 @@
+<?php
+echo "<h2>Belajar PHP itu mudah!</h2>";
+echo("Hello hafis code!<br>");
+echo "Disedang belajar PHP!<br>";
+echo "Ini ", "teks ", "yang ", "dibuat ", "terpisah.";
+
+echo "<br><br><br><strong>by M HAFIDZ RAMADHAN NST!";
+?>
