3968203cbfb633a83743affe710d41689a3cdb13
hafidznasution <rhafis709@gmail.com>
2024-08-28 08:04:03 +0700

    mengubah angka 01

---
+++ side name.php
@@ -0,0 +1,14 @@
+<?php // file: belajar-konstanta.php
+
+// membuat konstanta
+define('VERSION', '1.0.0');
+
+const SITE_NAME  = "hafidznasution";
+const BASE_URL    = "https://www.hafidznasution.com";
+
+// cetak nilai konstanta
+echo "Site name: " . SITE_NAME . "<br/>";
+echo "URL: " . BASE_URL . "<br/>";
+echo "Version: " . VERSION . "<br/>";
+
+echo "<br><br><br><strong>by M HAFIDZ RAMADHAN NST!";
