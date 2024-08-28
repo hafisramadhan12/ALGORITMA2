6c4768b53798e0a3c566ca1108d41a990e739be3
hafidznasution <rhafis709@gmail.com>
2024-08-28 10:37:58 +0700

    konversi tipe data di php

---
+++ 25 Konversi Tipe data di PHP.php
@@ -0,0 +1,7 @@
+<?php
+$var = "123.45";
+$intvar = "143";
+$intvar = intval($var);//mengubah menjadi integer
+$floatvar = floatval($var);// mengubah menjadi float
+echo $intvar;//output:123
+echo "<p>$floatvar";//output:123.45
