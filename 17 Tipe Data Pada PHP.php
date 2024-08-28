3ed57a90bf01277adb736e244ac192697279dc37
hafidznasution <rhafis709@gmail.com>
2024-08-28 09:32:37 +0700

    tipe data pada php

---
+++ 17 Tipe Data Pada PHP.php
@@ -0,0 +1,26 @@
+<?php
+
+// tipe data char (karakter)
+$jenis_kelamin = 'L';
+
+// tipe data string (teks)
+$nama_lengkap = "hafis";
+
+// tipe data integer
+$umur = 16;
+
+// tipe data float
+$berat = 50;
+
+// tipe data float
+$tinggi = 171;
+
+// tipe data boolean
+$siswa = false;
+
+echo "Nama: $nama_lengkap<br>";
+echo "Jenis Kelamin: $jenis_kelamin<br>";
+echo "Umur: $umur tahun<br>";
+echo "berat badan: $berat kg<br>";
+echo "tinggi badan: $tinggi cm<br>";
+echo "menikah: $siswa";
